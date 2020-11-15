3f519da2d03ae4a289e1a77aeae1a93843573757
SeeMorton1 <60202474+SeeMorton1@users.noreply.github.com>
2020-11-14 22:10:12 -0600

    Added AVLTree class

---
+++ AVLTree.h
@@ -0,0 +1,14 @@
+//
+// Created by Conner Morton on 11/14/2020.
+//
+
+#ifndef SEARCH_ENGINE_LIN_MORTON_AVLTREE_H
+#define SEARCH_ENGINE_LIN_MORTON_AVLTREE_H
+
+
+class AVLTree {
+
+};
+
+
+#endif //SEARCH_ENGINE_LIN_MORTON_AVLTREE_H
