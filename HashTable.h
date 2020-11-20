8ed575e6ab8a7c0efaec2d5491e7bf9093556548
SeeMorton1 <60202474+SeeMorton1@users.noreply.github.com>
2020-11-20 00:48:57 -0600

    HashTable done.

--- HashTable.h
+++ HashTable.h
@@ -30,7 +30,7 @@ public:
 class HashTable{
 private:
     int capacity;
-    list<pair<string,list<string>>> table;
+    list<pair<size_t, list<string>>> table;
 public:
     HashTable(){
         capacity=0;
@@ -39,35 +39,37 @@ public:
         capacity= table.size();
         return table.size();
     }
-    pair<string,list<string>> at(int n);
+
+    pair<size_t, list<string>> at(int n);
 
 
 
 
 
     bool isEmtpy()const;
-    void insertItem(const string& key, const string& value);
-  //  void removeItem(const K& key);
+
+    void insertItem(const string &key, const string &value);
+
+    void removeItem(const int &key);
   //  V& searchTable(const K& key);
 //    void printTable();
 };
 
-pair<string,list<string>> HashTable::at(int n) {
+pair<size_t, list<string>> HashTable::at(int n) {
     auto it = table.begin();
-    if(n<table.size()&&n>0){
-        advance(it,n);
-        pair<string,list<string>> p;
-        p.first=it->first;
-        p.second= it->second;
+    if (n < table.size() && n > 0) {
+        advance(it, n);
+        pair<size_t, list<string>> p;
+        p.first = it->first;
+        p.second = it->second;
         return p;
-    }else{
-        cout<<"out of range"<<endl;
-        pair<string,list<string>> p;
+    } else {
+        cout << "out of range" << endl;
+        pair<size_t, list<string>> p;
         p.first=table.begin()->first;
         p.second=table.begin()->second;
         return p;
     }
-
 }
 
 
@@ -78,10 +80,34 @@ bool HashTable::isEmtpy() const {
 void HashTable::insertItem(const string &key, const string &value) {
     hash<string> str_hash;
     size_t hashVal = str_hash(key);
+    bool exists = false;
+    for (auto &it : table) {
 
-    auto& cell = table.;
+        if (it.first == hashVal) {
+            exists = true;
+            it.second.push_back(value);
+        }
+    }
+    if (!exists) {
+        pair<size_t, list<string>> p;
+        list<string> ll;
+        ll.push_back(value);
+        p.second = ll;
+        p.first = hashVal;
+        table.push_back(p);
+    }
 
 
 }
 
+void HashTable::removeItem(const int &key) {
+
+}
+
 #endif //SEARCH_ENGINE_LIN_MORTON_HASHTABLE_H
+
+// TODO  Make the list actually a pair of ints for the hash code and the list of strings for the IDS
+// 0|List<ids>
+// 1|list<ids>
+//This works by assigning a has to a string. THIS WORKS
+
--- IndexNodesTest.cpp
+++ IndexNodesTest.cpp
@@ -4,6 +4,7 @@
 #include "AvLTree.h"
 #include "IndexNodesTest.h"
 #include "Index.h"
+#include "HashTable.h"
 //
 // Created by Conner Morton on 11/15/2020.
 //
@@ -22,7 +23,6 @@ void IndexNodesTest::runTests() {
     index2.setCount(2);
 
 
-
     AvLTree<Index> testTree;
     testTree.insert(index1);
     testTree.insert(index2);
@@ -32,6 +32,10 @@ void IndexNodesTest::runTests() {
 
     index3.setCount(7);
     testTree.insert(index3);
+
+    HashTable hash;
+    hash.insertItem("Zihao Lin", "Page 1");
+    hash.insertItem("Zihao Lin", "Page 2");
 
 
     cout << "Tests ran" << std::endl;
