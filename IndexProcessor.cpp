834370ecb097e3d2b603ded9a3b1131d4b1fcff6
Zihao Lin <zihaolin@smu.edu>
2020-11-23 11:22:24 -0600

    test

--- IndexProcessor.cpp
+++ IndexProcessor.cpp
@@ -12,16 +12,25 @@ void IndexProcessor::createIndex(DocParser &doc) {
         for(auto& word:words){
             Index c;
             c.setWord(word);
-            if(wordIndex.isFound(wordIndex.getRoot(),c)){
-                AvLNode<Index> *f = wordIndex.searchUtil(wordIndex.getRoot(), c);
+            if (wordIndex.isFound(wordIndex.getRoot(), c)) {
+                AvLNode<Index> *f = wordIndex.searchAlgo(wordIndex.getRoot(), c);
                 f->getData().addCount();
                 f->getData().addID(it.returnJsonFileName());
 
-            }else{
+            } else {
                 c.addID(it.returnJsonFileName());
                 wordIndex.insert(c);
             }
         }
 
     }
+}
+
+list<string> IndexProcessor::findWord(string w) {
+    Index sea;
+    sea.setWord(w);
+    list<string> i = wordIndex.searchAlgo(wordIndex.getRoot(), sea)->getData().getIDs();
+
+    return i;
+
 }
